ddec7dadf4b6cbf69e0017c87c2ae6b11fdd8d24
matochichap <ruijia.chan@gmail.com>
2025-12-18 19:02:52 +0800

    remove misleading footer

--- ui/src/app/auth/login/page.tsx
+++ ui/src/app/auth/login/page.tsx
@@ -59,22 +59,12 @@ export default function Login() {
                         </div>
                     </button>
 
-                    {/* Footer */}
-                    <div className="mt-8 text-center">
-                        <p className="text-sm text-gray-500">
-                            By signing in, you agree to our{' '}
-                            <a href="#" className="text-blue-600 hover:text-blue-500 underline">Terms of Service</a>
-                            {' '}and{' '}
-                            <a href="#" className="text-blue-600 hover:text-blue-500 underline">Privacy Policy</a>
+                    {/* Additional Info */}
+                    <div className="mt-6 text-center">
+                        <p className="text-sm text-gray-600">
+                            Secure authentication powered by Google
                         </p>
                     </div>
-                </div>
-
-                {/* Additional Info */}
-                <div className="mt-6 text-center">
-                    <p className="text-sm text-gray-600">
-                        Secure authentication powered by Google
-                    </p>
                 </div>
             </div>
         </div>
